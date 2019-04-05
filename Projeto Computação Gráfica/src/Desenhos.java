1ee758ea893f682b8bd3402852303ce8da6cd885
unknown <elthonferreira18@gmail.com>
2019-04-05 11:03:33 -0300

    alterações na classe desenho

--- Projeto Computação Gráfica/src/Desenhos.java
+++ Projeto Computação Gráfica/src/Desenhos.java
@@ -1,16 +1,22 @@
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Desenhos {
 
 	public static void le_arquivo (String nome_arquivo) throws IOException {
 		
+		// Leitura dos arquivos
 		String path = "formas/" + nome_arquivo + ".byu";
-		
 		BufferedReader leitor = new BufferedReader(new FileReader(path));
 		
+		// Get dados
+		String line = leitor.readLine();
+		String split [] = line.split(" ");
 		
+		ArrayList<Point> points = new ArrayList<>(); 
 	}
 }
