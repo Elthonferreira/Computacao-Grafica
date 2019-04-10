c500d249334780c0adb1ea803df6352526d3c14b
Elthon Ferreira <elthonferreira18@gmail.com>
2019-04-09 21:09:00 -0300

    add classe salvar

---
+++ Projeto Computação Gráfica/src/Beans/Salvar.java
@@ -0,0 +1,17 @@
+package Beans;
+
+import java.util.ArrayList;
+
+public class Salvar {
+
+	public ArrayList<Ponto> pontos;
+	public ArrayList<Triangulo> triangulos;
+	
+	public Salvar() {
+		this.pontos = new ArrayList<>();
+		this.triangulos = new ArrayList<>();
+	}
+	
+	
+
+}
--- Projeto Computação Gráfica/src/Desenhos.java
+++ Projeto Computação Gráfica/src/Desenhos.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import Beans.Ponto;
+import Beans.Triangulo;
 
 public class Desenhos {
 
@@ -18,16 +19,31 @@ public class Desenhos {
 		System.out.println("" + aux2[0] + "\n" + aux2[1]);
 		int qtd_pontos = Integer.parseInt(aux2[0]);
 		int qtd_triangulos = Integer.parseInt(aux2[1]);
+		
 		ArrayList<Ponto> pontos = new ArrayList<>();
-
+		ArrayList<Triangulo> triangulos = new ArrayList<>();
+				
 		for (int i = 0; i < qtd_pontos; i++) {
-			
+			aux = leitor.readLine();
+			aux2 = aux.split(" ");
+			Ponto p = new Ponto(Double.parseDouble(aux2[0]), 
+								Double.parseDouble(aux2[1]), 
+								Double.parseDouble(aux2[2]));
+			pontos.add(p);	
+		}
+		
+		for (int i = 0; i < qtd_triangulos; i++) {
+			aux = leitor.readLine();
+			aux2 = aux.split(" ");
+			Triangulo t = new Triangulo(pontos.get(Integer.parseInt(aux2[0]) - 1), 
+										pontos.get(Integer.parseInt(aux2[1]) - 1),
+										pontos.get(Integer.parseInt(aux2[2]) - 1));
 		}
 	}
 		
 	public static void main (String args[]) {
 		try {
-			le_arquivo("maca");
+			le_arquivo("vaso");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
