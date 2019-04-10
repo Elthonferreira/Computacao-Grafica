909e598b9b464de3987486afb88ce5aa8caa97ed
Elthon Ferreira <elthonferreira18@gmail.com>
2019-04-09 21:14:44 -0300

    identação classe salvar

--- Projeto Computação Gráfica/src/Beans/Salvar.java
+++ Projeto Computação Gráfica/src/Beans/Salvar.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 
 public class Salvar {
 
+	// Atributos
 	public ArrayList<Ponto> pontos;
 	public ArrayList<Triangulo> triangulos;
 	
+	// Construtor
 	public Salvar(ArrayList<Ponto> pontos, ArrayList<Triangulo> triangulos) {
 		this.pontos = pontos;
 		this.triangulos = triangulos;
 	}
-	
-	
-
 }
