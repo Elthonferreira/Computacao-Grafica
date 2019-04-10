122973536aa1ab2208af2b5bd90d63968f868be1
Elthon Ferreira <elthonferreira18@gmail.com>
2019-04-10 00:26:31 -0300

    questão 2 concluída com ajuda de fabão

--- Projeto Computação Gráfica/src/Desenhos.java
+++ Projeto Computação Gráfica/src/Desenhos.java
@@ -13,48 +13,53 @@ public class Desenhos {
 
 		// Leitura dos arquivos
 		String path = "formas/" + nome_arquivo + ".byu";
-		BufferedReader leitor = new BufferedReader(new FileReader(path));
+		BufferedReader leitor = new BufferedReader(new FileReader(path)); // Leitor
 
-		String aux = leitor.readLine();
-		String aux2[] = aux.split(" ");
-		System.out.println("" + aux2[0] + "\n" + aux2[1]);
-		int qtd_pontos = Integer.parseInt(aux2[0]);
+		String aux = leitor.readLine(); // Lê valores e guarda no vetor aux
+		String aux2[] = aux.split(" "); // Separa com espaços e guarda no vetor aux2
+		//System.out.println("" + aux2[0] + "\n" + aux2[1]);
+		int qtd_pontos = Integer.parseInt(aux2[0]); 
 		int qtd_triangulos = Integer.parseInt(aux2[1]);
 
 		ArrayList<Ponto> pontos = new ArrayList<>();
 		ArrayList<Triangulo> triangulos = new ArrayList<>();
 
+		// Adicionando os pontos no array de pontos
 		for (int i = 0; i < qtd_pontos; i++) {
 			aux = leitor.readLine();
 			aux2 = aux.split(" ");
-			Ponto p = new Ponto(Double.parseDouble(aux2[0]), Double.parseDouble(aux2[1]), Double.parseDouble(aux2[2]));
+			Ponto p = new Ponto(Double.parseDouble(aux2[0]), 
+								Double.parseDouble(aux2[1]), 
+								Double.parseDouble(aux2[2]));
 			pontos.add(p);
 		}
-
+		
+		// Adicionando os triângulos no array de triângulos
 		for (int i = 0; i < qtd_triangulos; i++) {
 			aux = leitor.readLine();
 			aux2 = aux.split(" ");
 			Triangulo t = new Triangulo(pontos.get(Integer.parseInt(aux2[0]) - 1),
-					pontos.get(Integer.parseInt(aux2[1]) - 1), pontos.get(Integer.parseInt(aux2[2]) - 1));
+										pontos.get(Integer.parseInt(aux2[1]) - 1), 
+										pontos.get(Integer.parseInt(aux2[2]) - 1));
 			triangulos.add(t);
 		}
 		Salvar save = new Salvar(pontos, triangulos);
 		return save;
 	}
-
-	public static Salvar normalização (Salvar s, int width, int length) {
+	// Função de normalização
+	public static Salvar normalizacao (Salvar s, int width, int length) {
 		double minx = s.pontos.get(0).x;
 		double miny = s.pontos.get(0).y;
 		double maxx = s.pontos.get(0).x;
 		double maxy = s.pontos.get(0).y;
-		
+		// Comparando máx e min
 		for (int i = 0; i < s.pontos.size(); i++) { 
 			if (s.pontos.get(i).x < minx) minx = s.pontos.get(i).x;
 			if (s.pontos.get(i).y < miny) miny = s.pontos.get(i).y;
 			if (s.pontos.get(i).x > maxx) maxx = s.pontos.get(i).x;
 			if (s.pontos.get(i).y > maxy) maxy = s.pontos.get(i).y;
 		}
-
+		// Fórmula
 		for (int j = 0; j < s.pontos.size(); j++) {
 			s.pontos.get(j).x = ((s.pontos.get(j).x - minx)/(maxx - minx))*(width - 1);
 			s.pontos.get(j).y = ((s.pontos.get(j).y - miny)/(maxy - miny))*(length - 1);
--- Projeto Computação Gráfica/src/JavaFX.java
+++ Projeto Computação Gráfica/src/JavaFX.java
@@ -4,6 +4,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class JavaFX extends Application{
@@ -14,16 +15,21 @@ public class JavaFX extends Application{
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		int width = 500, height = 500;
+		int width = 1100, height = 650;
 		Canvas canvas = new Canvas(width, height);
 		GraphicsContext grafico = canvas.getGraphicsContext2D();
-		// grafico.fillOval(250, 250, 30, 30); // Desenhe uma bola
+		// grafico.fillRect(250, 250, 30, 30); // Desenhe uma bola
 		
-		Salvar s1 = Desenhos.le_arquivo("vaso");
+		Salvar draw = Desenhos.le_arquivo("vaso");
+		draw = Desenhos.normalizacao(draw, width, height);
 		
+		grafico.fillRect(0, 0, width, height); // Pintar um pixel gigante do tamanho da tela
+		grafico.setFill(Color.WHITE);		   // Cor branca
 		
-		
-		
+		for(int i = 10; i < draw.pontos.size(); i++) { // Pintar tela
+			grafico.fillRect(draw.pontos.get(i).x, draw.pontos.get(i).y, 50, 50); // Cords (x, y, largura pixel, altura pixel)
+		}
+				
 		Group grupo = new Group(canvas);
 		Scene cena = new Scene(grupo);
 		
