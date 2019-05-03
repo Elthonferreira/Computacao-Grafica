c9ce3db93e69bab4b3b957e2504d278382943950
Elthon Ferreira <elthonferreira18@gmail.com>
2019-05-02 21:24:59 -0300

    Inicio classe camera

---
+++ Projeto Computação Gráfica/src/Beans/Camera.java
@@ -0,0 +1,22 @@
+package Beans;
+
+import java.io.BufferedReader;
+
+public class Camera {
+	
+	public double v [];
+	public double u [];
+	public double n [];
+	
+	public Ponto c;
+	public double d, hx, hy;
+	public BufferedReader leitor;
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
